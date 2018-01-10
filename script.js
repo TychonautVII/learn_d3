69ef0c6cd70ab8d4d91d682cc0188af9c97c6d58
Peter Mariani <peter@Peters-MacBook-Pro.local>
2018-01-09 20:29:20 -0500

    Should be a hello world d3 program

---
+++ script.js
@@ -0,0 +1,10 @@
+var w=960,h=500,
+svg=d3.select("#chart") //css selector refering to the html object with an id attribute chart
+.append("svg") // an "svg" variable that holds the top-level svg container, which resides in a div element called "chart"
+.attr("width",w)
+.attr("height",h);
+ 
+var text=svg
+.append("text")
+.text("hello world")
+.attr("y",50);
