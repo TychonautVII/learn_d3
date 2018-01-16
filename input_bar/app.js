e7e7bd875fc53cd70b16ccb93bbdb017d23eb066
Peter Mariani <peter@Peters-MacBook-Pro.local>
2018-01-16 13:32:49 -0500

    refactored directory structure for clarity and easy use of angular

--- input_bar/app.js
+++ input_bar/app.js
@@ -1,91 +1,103 @@
-// ----------------------------------------
-//  Notes
-// ----------------------------------------
-
-/*
-About Java Script:
-
-Client Side language, runs on the client, as opposed to a server side language
-
-Node.js is an open-source, cross-platform JavaScript run-time environment for executing JavaScript code server-side.
-
-npm is a package manager for the JavaScript programming language
-
- */
-
 "use strict";  // Code that enforces modern javascript
+(function(){
+    let app = angular.module('input_bar',[ ]);
 
-let margin = {top: 40, right: 20, bottom: 30, left: 40};
+    console.log("This Runs");
 
-let width= 300 - margin.left - margin.right;
-let height = 700 - margin.top - margin.bottom;
+    app.controller('Plotter',function(){
+        // ----------------------------------------
+        //  Init the Data
+        // ----------------------------------------
+                this.sales = [
+                    { product: 'Hoodie',  count: 7 },
+                    { product: 'Jacket',  count: 6 },
+                    { product: 'Snuggie', count: 9 },
+                ];
 
-// ----------------------------------------
-// Setting Up the Scales
-// ----------------------------------------
+                this.plot = function(){
+                    let margin = {top: 40, right: 20, bottom: 30, left: 40};
 
-let x_scale = d3.scaleBand()
-    .range([0, width])
-    .padding(0.1);
+                    let width= 700 - margin.left - margin.right;
+                    let height = 300 - margin.top - margin.bottom;
 
-let y_scale = d3.scaleLinear()
-  .range([height, 0]); //Range is in pixel space, reversed because high down in svg
+                    // ----------------------------------------
+                    // Setting Up the Scales
+                    // ----------------------------------------
 
+                    let x_scale = d3.scaleBand()
+                        .range([0, width])
+                        .padding(0.1);
 
-// ----------------------------------------
-// Sizing and Placing the Chart
-// ----------------------------------------
-
-let svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-// ----------------------------------------
-//  Getting the Data
-// ----------------------------------------
-
-let sales = [
-    { product: 'Hoodie',  count: 7 },
-    { product: 'Jacket',  count: 6 },
-    { product: 'Snuggie', count: 9 },
-];
-
-// ----------------------------------------
-// Binding the Data
-// ----------------------------------------
-
-// Scale the range of the data in the domains
-x_scale.domain(sales.map(function(d) { return d.product; }));
-y_scale.domain([0, d3.max(sales, function(d) { return d.count; })]);
-
-svg.selectAll(".bar")
-    .data(sales)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) {return x_scale(d.product);})
-    .attr("width", x_scale.bandwidth() )
-    .attr("y", function(d) {return y_scale(d.count);})
-    .attr("height", function(d) { return  height - y_scale(d.count); });
-
-// ----------------------------------------
-// Sizing and Placing the Chart
-// ----------------------------------------
-
-// add the x Axis
-svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x_scale));
-
-// add the y Axis
-svg.append("g")
-    .call(d3.axisLeft(y_scale));
+                    let y_scale = d3.scaleLinear()
+                        .range([height, 0]); //Range is in pixel space, reversed because high down in svg
 
 
-console.log("End of Program");
+                    // ----------------------------------------
+                    // Sizing and Placing the Chart
+                    // ----------------------------------------
+
+                    let svg = d3.select("body").append("svg")
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                    // ----------------------------------------
+                    // Binding the Data
+                    // ----------------------------------------
+
+                    // Scale the range of the data in the domains
+                    x_scale.domain(this.sales.map(function(d) { return d.product; }));
+                    y_scale.domain([0, d3.max(this.sales, function(d) { return d.count; })]);
+
+                    svg.selectAll(".bar")
+                        .data(this.sales)
+                        .enter()
+                        .append("rect")
+                        .attr("class", "bar")
+                        .attr("x", function(d) {return x_scale(d.product);})
+                        .attr("width", x_scale.bandwidth() )
+                        .attr("y", function(d) {return y_scale(d.count);})
+                        .attr("height", function(d) { return  height - y_scale(d.count); });
+
+                    // ----------------------------------------
+                    // Sizing and Placing the Chart
+                    // ----------------------------------------
+
+                    // add the x Axis
+                    svg.append("g")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(d3.axisBottom(x_scale));
+
+                    // add the y Axis
+                    svg.append("g")
+                        .call(d3.axisLeft(y_scale));
+
+
+                    console.log("End of Program");
+                }
+
+                this.plot()
+
+
+
+    });
+
+    app.controller('Inputer',function(){
+        // This is the code that will be executed when the controller is called
+        this.input = {};
+        this.addInput = function(plotter){
+            plotter.sales.push(this.input);
+            plotter.plot()
+            this.input = {};
+        }
+
+    });
+
+
+})();
+
+
 
 
 
